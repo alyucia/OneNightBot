b8fe1b45f1ba449bd6aba0ced44ccba52f0fa4b0
Aly <althong@gmail.com>
2017-09-15 16:45:16 -0700

    quick commit

--- src/bot.java
+++ src/bot.java
@@ -16,6 +16,7 @@ class Game{
     private Role[] unused = new Role[3];
     public Game(int nplay){
         players = new Player[nplay];
+        playerOrder = new Player[nplay];
         roles = new Role[nplay + 3];
         createRoles();
         shuffleRoles();
@@ -28,7 +29,6 @@ class Game{
         String input = sc.nextLine();
         String[] inputArray = input.split(", ");
         for (int i = 0; i < inputArray.length; i++){
-            System.out.println(inputArray[i]);
             switch(Integer.parseInt(inputArray[i])){
                 case 1: roles[i] = new Doppelganger();
                 case 2: roles[i] = new Werewolf();
@@ -51,9 +51,11 @@ class Game{
 
     }
     private void setOrder(){
+        for (int i = 0; i < players.length; i++)
+            playerOrder[i] = players[i];
         Arrays.sort(playerOrder);
     }
-    private void shuffleRoles()    {
+    private void shuffleRoles(){
         int index;
         Role temp;
         Random random = new Random();
