26b50e1ff6fc92a65b6f25e29a2f72bf5e8f3a0a
Aly <althong@gmail.com>
2017-09-15 15:59:00 -0700

    compiled ver

--- src/main/java/bot/goonbaya/bot.java
+++ src/main/java/bot/goonbaya/bot.java
@@ -1,4 +1,4 @@
-package bot.goonbaya
+package bot.goonbaya;
 
 import java.util.*;
 
@@ -61,12 +61,18 @@ class Role{
     private boolean were;
     private int order;
     private String name;
-    public Role(){
-        were = false;
+    public Role(String n, boolean w, int o){
+        name = n;
+        were = w;
+        order = o;
+
     }
     public void doAction(Player player, Player[] players, Role[] unused){}
     public boolean getWere(){
         return were;
+    }
+    public void setWere(boolean w){
+        this.were = w;
     }
     public int getOrder(){
         return order;
@@ -79,20 +85,18 @@ class Role{
 
 class Doppelganger extends Role{
     public Doppelganger(){
-        name = "Doppelganger";
-        were = false;
-        order = 1;
+        super("Doppelganger", false, 1);
     }
     public void doAction(Player player, Player[] players, Role[] unused){
         System.out.println("Pick a player to copy");
-        int holder = nextInt();
+        int holder = sc.nextInt();
         System.out.println("You are now a" + players[holder].getRole().getName());
         if (players[holder].getRole().getName().equals("Insomniac")){
             //figure out insomniac shit here
             return;
         }
         else if (players[holder].getRole().getWere()){
-            this.were = true;
+            this.setWere(true);
         }
         players[holder].getRole().doAction(player, players, unused);
     }
@@ -100,13 +104,11 @@ class Doppelganger extends Role{
 
 class Werewolf extends Role{
     public Werewolf(){
-        name = "Werewolf";
-        were = true;
-        order = 2;
+        super("Werewolf", true, 2);
     }
     public void doAction(Player player, Player[] players, Role[] unused){
         boolean oneWolf = true;
-        for (int i = 0; i < players.length(); i++) {
+        for (int i = 0; i < players.length; i++) {
             if(players[i].getRole().getWere() && players[i].getUser() != player.getUser()){
                 System.out.println(players[i].getUser() + " is a Werewolf");
                 oneWolf = false;
@@ -114,7 +116,7 @@ class Werewolf extends Role{
         }
         if (oneWolf){
             System.out.println("Choose one card from the middle.");
-            holder = sc.nextInt();
+            int holder = sc.nextInt();
             System.out.println("That card is " + unused[holder].getName());
         }
     }
@@ -122,12 +124,10 @@ class Werewolf extends Role{
 
 class Minion extends Role{
     public Minion(){
-        name = "Minion";
-        were = false;
-        order = 3;
+        super("Minion", false, 3);
     }
     public void doAction(Player player, Player[] players, Role[] unused){
-        for (int i = 0; i < players.length(); i++) {
+        for (int i = 0; i < players.length; i++) {
             if(players[i].getRole().getName().equals("Werewolf")){
                 System.out.println(players[i].getUser() + " is a Werewolf");
             }
@@ -137,16 +137,14 @@ class Minion extends Role{
 
 class Mason extends Role{
     public Mason(){
-        name = "Mason";
-        were = false;
-        order = 4;
+        super("Mason", false, 4);
     }
     public void doAction(Player player, Player[] players, Role[] unused){
         boolean oneMason = true;
-        for (int i = 0; i < players.length(); i++) {
+        for (int i = 0; i < players.length; i++) {
             if(players[i].getRole().getName().equals("Mason") && players[i].getUser() != player.getUser()){
                 System.out.println(players[i].getUser() + " is a Mason");
-                oneWolf = false;
+                oneMason = false;
             }
         }
         if (oneMason){
@@ -157,9 +155,7 @@ class Mason extends Role{
 
 class Seer extends Role{
     public Seer(){
-        name = "Seer";
-        were = false;
-        order = 5;
+        super("Seer", false, 5);
     }
     public void doAction(Player player, Player[] players, Role[] unused){
         System.out.println("Choose a player, or middle cards");
@@ -180,9 +176,7 @@ class Seer extends Role{
 
 class Robber extends Role{
     public Robber(){
-        name = "Robber";
-        were = false;
-        order = 6;
+        super("Robber", false, 6);
     }
     public void doAction(Player player, Player[] players, Role[] unused){
         System.out.println("Pick a user to switch with.");
@@ -196,9 +190,7 @@ class Robber extends Role{
 
 class Troublemaker extends Role{
     public Troublemaker(){
-        name = "Troublemaker";
-        were = false;
-        order = 7;
+        super("Troublemaker", false, 7);
     }
     public void doAction(Player player, Player[] players, Role[] unused){
         System.out.println("Pick two players to switch");
@@ -213,9 +205,7 @@ class Troublemaker extends Role{
 
 class Drunk extends Role{
     public Drunk(){
-        name = "Drunk";
-        were = false;
-        order = 8;
+        super("Drunk", false, 8);
     }
     public void doAction(Player player, Player[] players, Role[] unused){
         System.out.println("Pick an unused role to switch with.");
@@ -228,9 +218,7 @@ class Drunk extends Role{
 
 class Insomniac extends Role{
     public Insomniac(){
-        name = "Insomniac";
-        were = false;
-        order = 9;
+        super("Insomniac", false, 9);
     }
     public void doAction(Player player, Player[] players, Role[] unused){
         System.out.println("You are " + player.getRole().getName());
@@ -239,25 +227,19 @@ class Insomniac extends Role{
 
 class Tanner extends Role{
     public Tanner(){
-        name = "Tanner";
-        were = false;
-        order = 0;
+        super("Tanner", false, 0);
     }
 
 }
 
 class Hunter extends Role{
     public Hunter(){
-        name = "Hunter";
-        were = false;
-        order = 0;
+        super("Hunter", false, 0);
     }
 }
 
 class Villager extends Role{
     public Villager(){
-        name = "Villager";
-        were = false;
-        order = 0;
+        super("Villager", false, 0);
     }
 }
